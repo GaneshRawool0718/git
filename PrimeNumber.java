197fff7d187ff2bf9937edd1af2b3fd029181667
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-06-20 13:04:27 +0530

    feat(dsa): add prime number code implementation

---
+++ PrimeNumber.java
@@ -0,0 +1,36 @@
+package Day02;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    // This program checks if a given number is prime or not.
+    // A prime number is a natural number greater than 1 that cannot be formed by multiplying
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user 
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+        sc.close();
+    }
+
+    // Optimized method to check for prime
+    static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+        int sqrt = (int)Math.sqrt(number);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+    
+
+}
